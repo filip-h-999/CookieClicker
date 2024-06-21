5f79a60e06f9a4ca90e0c4ad22dc530acf0a8337
filip-h-999 <filip.hrenovcik@gmail.com>
2024-06-21 13:05:15 +0200

    refactor: Update GUI initialization to include additional parameters and help for next lvls

--- gui.py
+++ gui.py
@@ -2,31 +2,38 @@ import pygame
 
 
 class GUI:
-    def __init__(self, window):
+    def __init__(self, window, path, shopBgPath, shopSize, shopFramePath, frameIcon, upgradeFramePath):
         self.window = window
         self.black = 0, 0, 0
         self.BACK_WIDTH = 800 * 1.5
         self.BACK_HEIGHT = 600 * 1.5
 
-        backImage = pygame.image.load(r"assets/images/b2.png")
-        self.back = pygame.transform.scale(backImage, (self.BACK_WIDTH, self.BACK_HEIGHT))
-        frameImage = pygame.image.load(r"assets/images/frame.png")
-        self.frame = pygame.transform.scale(frameImage, (400, 600))
-        shopImage = pygame.image.load(r"assets/images/shop.png")
-        self.shop = pygame.transform.scale(shopImage, (300, 100))
-        frame2Image = pygame.image.load(r"assets/images/frame2.png")
-        self.frame2 = pygame.transform.scale(frame2Image, (500, 150))
-        upgradeFrameImage = pygame.image.load(r"assets/images/upgradesFrame.png")
-        self.upgradeFrame = pygame.transform.scale(upgradeFrameImage, (300, 100))
+        self.backgroundPath = path
+        self.shopBgPath = shopBgPath
+        self.shopSize = shopSize
+        self.shopFramePath = shopFramePath
+        self.upgradesFramePath = upgradeFramePath
+        # backImage = pygame.image.load(r"assets/images/backgoundWood.png")
+        self.back = pygame.transform.scale(path, (self.BACK_WIDTH, self.BACK_HEIGHT))
+        # frameImage = pygame.image.load(r"assets/images/frame.png")
+        self.frame = pygame.transform.scale(shopBgPath, (shopSize))
+        # shopImage = pygame.image.load(r"assets/images/shop.png")
+        self.shop = pygame.transform.scale(shopFramePath, (300, 100))
+        # frame2Image = pygame.image.load(r"assets/images/frame2.png")
+        self.frame2 = pygame.transform.scale(frameIcon, (500, 150))
+        # upgradeFrameImage = pygame.image.load(r"assets/images/upgradesFrame.png")
+        self.upgradeFrame = pygame.transform.scale(upgradeFramePath, (300, 100))
 
     def drawBackG(self):
         self.window.blit(self.back, (0, 0))
 
-    def drawFrame(self):
+    def drawFrame(self, shopPoss, upgradePoss):
+        self.shopPossition = shopPoss
+        self.shopPossition = upgradePoss
+        self.window.blit(self.frame, (shopPoss))
         self.window.blit(self.shop, (850, 10))
-        self.window.blit(self.frame, (815, 60))
         self.window.blit(self.frame2, (350, 80))
         self.window.blit(self.frame2, (350, 210))
         self.window.blit(self.frame2, (350, 340))
         self.window.blit(self.frame2, (350, 470))
-        self.window.blit(self.upgradeFrame, (442, 10))
+        self.window.blit(self.upgradeFrame, (upgradePoss))
