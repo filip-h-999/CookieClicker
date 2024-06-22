3e930de754d2793e9fc5ec15437dadd7bc7f16a6
filip-h-999 <filip.hrenovcik@gmail.com>
2024-06-22 13:17:44 +0200

    refactor: Update Cookie class initialization to include image path, width, and height(for better lvl improvement)

--- cookie.py
+++ cookie.py
@@ -2,8 +2,9 @@ import pygame
 from pygame import mixer
 
 
-def cookieSound():
-    clickSound = r"assets/sounds/cookieS.mp3"
+def cookieSound(clickSound):
+    # clickSoundCookie = r"assets/sounds/cookieS.mp3"
+    # clickSoundBattery = r"assets/sounds/batteryS.mp3"
     mixer.music.load(clickSound)
     mixer.music.set_volume(0.2)
     mixer.music.play()
@@ -12,23 +13,24 @@ def cookieSound():
 
 
 class Cookie:
-    def __init__(self, window):
+    def __init__(self, window, imgPath, width, height):
         self.window = window
-        self.COOKIE_WIGHT = 330
-        self.COOKIE_HEIGHT = 330
+        self.COOKIE_WIGHT = width
+        self.COOKIE_HEIGHT = height
         self.black = 0, 0, 0
         # self.score = 0
         # self.increaseS = 1
 
-        self.cookieImage = pygame.image.load(r"assets/images/cookie3.png")
-        self.cookie = pygame.transform.scale(self.cookieImage, (self.COOKIE_WIGHT, self.COOKIE_HEIGHT))
-        self.cookie2 = pygame.transform.scale(self.cookieImage, (self.COOKIE_WIGHT + 10, self.COOKIE_HEIGHT + 10))
+        self.cookieImage = imgPath
+        self.cookie = pygame.transform.scale(imgPath, (self.COOKIE_WIGHT, self.COOKIE_HEIGHT))
+        self.cookie2 = pygame.transform.scale(imgPath, (self.COOKIE_WIGHT + 10, self.COOKIE_HEIGHT + 10))
         self.rect = pygame.Rect(31, 190, self.COOKIE_HEIGHT, self.COOKIE_WIGHT)
         self.collisionRect = self.rect.inflate(-90, -90)
 
 
-    def drawCookie(self):
-        self.window.blit(self.cookie, (31, 190))
+    def drawCookie(self, poss):
+        self.poss = poss
+        self.window.blit(self.cookie, (poss))
 
 
     def drawBigCookies(self):
@@ -38,8 +40,8 @@ class Cookie:
     # def increaseScore(self, n):
     #     self.score += n
 
-    def clickCookie(self):
-        cookieSound()
+    def clickCookie(self, clickSound):
+        cookieSound(clickSound)
         # self.increaseScore(self.increaseS)
 
 
