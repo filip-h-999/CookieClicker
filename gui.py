408877dfa3464b54e84d2830bc8d5952d9d0c1d4
filip-h-999 <filip.hrenovcik@gmail.com>
2024-09-22 11:44:07 +0200

    Refactor GUI class initialization to include size parameters for shop frame and upgrade frame

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class GUI:
-    def __init__(self, window, path, shopBgPath, shopSize, shopFramePath, frameIcon, upgradeFramePath, posXY):
+    def __init__(self, window, path, shopBgPath, shopSize, shopFramePath, shopFrameSize, frameIcon, upgradeFramePath, upgradeSize):
         self.window = window
         self.black = 0, 0, 0
         self.BACK_WIDTH = 800 * 1.5
@@ -16,24 +16,24 @@ class GUI:
         # backImage = pygame.image.load(r"assets/images/backgoundWood.png")
         self.back = pygame.transform.scale(path, (self.BACK_WIDTH, self.BACK_HEIGHT))
         # frameImage = pygame.image.load(r"assets/images/frame.png")
-        self.frame = pygame.transform.scale(shopBgPath, (shopSize))
+        self.shopBackground = pygame.transform.scale(shopBgPath, (shopSize))
         # shopImage = pygame.image.load(r"assets/images/shop.png")
-        self.shop = pygame.transform.scale(shopFramePath, (300, 100))
+        self.shopFrame = pygame.transform.scale(shopFramePath, (shopFrameSize))
         # frame2Image = pygame.image.load(r"assets/images/frame2.png")
-        self.frame2 = pygame.transform.scale(frameIcon, (500, 150))
+        self.upgradeButtons = pygame.transform.scale(frameIcon, (500, 150))
         # upgradeFrameImage = pygame.image.load(r"assets/images/upgradesFrame.png")
-        self.upgradeFrame = pygame.transform.scale(upgradeFramePath, (posXY))
+        self.upgradeFrame = pygame.transform.scale(upgradeFramePath, (upgradeSize))
 
     def drawBackG(self):
         self.window.blit(self.back, (0, 0))
 
-    def drawFrame(self, shopPoss, upgradePoss):
+    def drawFrame(self, shopPoss, upgradePoss, shopFramePoss):
         self.shopPossition = shopPoss
         self.shopPossition = upgradePoss
-        self.window.blit(self.frame, (shopPoss))
-        self.window.blit(self.shop, (850, 10))
-        self.window.blit(self.frame2, (350, 80))
-        self.window.blit(self.frame2, (350, 210))
-        self.window.blit(self.frame2, (350, 340))
-        self.window.blit(self.frame2, (350, 470))
+        self.window.blit(self.shopBackground, (shopPoss))
+        self.window.blit(self.shopFrame, (shopFramePoss))
+        self.window.blit(self.upgradeButtons, (350, 80))
+        self.window.blit(self.upgradeButtons, (350, 210))
+        self.window.blit(self.upgradeButtons, (350, 340))
+        self.window.blit(self.upgradeButtons, (350, 470))
         self.window.blit(self.upgradeFrame, (upgradePoss))
