8ca67f4db91004b9bd674fb7e8b3dfffd3ecbc74
filip-h-999 <filip.hrenovcik@gmail.com>
2024-09-23 11:51:30 +0200

    changing color for visibility

--- playtime.py
+++ playtime.py
@@ -14,5 +14,5 @@ class PlaytimeDisplay:
         minutes = int((playtime / 60000) % 60)
         hours = int((playtime / 3600000) % 24)
         out = '{hours:02d}:{minutes:02d}:{seconds:02d}'.format(hours=hours, minutes=minutes, seconds=seconds)
-        self.font.render_to(self.window, (80, 630), out, pygame.Color('black'))
+        self.font.render_to(self.window, (80, 630), out, pygame.Color('white'))
         # pygame.display.flip()
